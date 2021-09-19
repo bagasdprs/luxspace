f2271687f81226d26178a11475755a49da685772
bagasdprs <bagasdprs@gmail.com>
2021-09-19 20:38:08 +0700

    deploy to netlify

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,4 +1,7 @@
-import { addClass, removeClass } from "./utils-class";
+import {
+  addClass,
+  removeClass
+} from "./utils-class";
 
 function accordion() {
   const accordionContainer = document.getElementsByClassName("accordion");
@@ -13,7 +16,9 @@ function accordion() {
     );
 
     button.style.top = "50%";
-    button.innerHTML = `<svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L9.75 7.5L18.5 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+    button.innerHTML = `<svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 1L9.75 7.5L18.5 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`
 
     const ulList = e.getElementsByTagName("ul")[0];
     addClass(ulList, "transition duration-200");
